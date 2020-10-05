aa7ea31d9792f8a20885beb91b9e26bf9ad6a9ae
123 <git config --global user.name 123a.aladov@codefresh.io>
2020-10-05 14:59:49 +0300

    CR-543 - Agent version

---
+++ installer/cmd/install_test.go
@@ -0,0 +1,25 @@
+package cmd
+
+import (
+	"testing"
+)
+
+func TestGetVersionFromContentString(t *testing.T) {
+	expectedVersion := "0.1.2"
+	testPayload := []string{
+		"0.1.2",
+		" 0.1.2  ",
+		" 0.1.2",
+		"0.1.2   ",
+		"\t0.1.2   \n",
+		"\t0.1.2\n",
+		"\t0.1.2",
+	}
+
+	for _, payload := range testPayload {
+		version := getVersionFromContentString(payload)
+		if version != expectedVersion {
+			t.Errorf("'GetVersionFromContentString' check version failed, expected: %v, got: %v", expectedVersion, version)
+		}
+	}
+}
