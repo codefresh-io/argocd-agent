89cb78fcb87b285a1158f5f36f8a3291d04df68c
123 <git config --global user.name 123a.aladov@codefresh.io>
2020-10-05 15:01:43 +0300

    CR-543 - Agent version

--- installer/cmd/install.go
+++ installer/cmd/install.go
@@ -268,11 +268,14 @@ func getAgentVersion() string {
 		fmt.Errorf(err.Error())
 		return ""
 	}else{
-		version := strings.TrimFunc(string(content), func (r rune) bool {
-			_r := string(r)
-			return _r == " " || _r =="\n" || _r =="\t" || _r =="\r"
-		})
+		version := getVersionFromContentString(string(content))
 		return version
 	}
+}
 
+func getVersionFromContentString(content string) string{
+	return strings.TrimFunc(content, func (r rune) bool {
+		_r := string(r)
+		return _r == " " || _r =="\n" || _r =="\t" || _r =="\r"
+	})
 }
