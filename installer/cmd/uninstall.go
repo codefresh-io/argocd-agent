3aa202bc1d311841a84f1b3cf08fcbba46b75fc4
123 <git config --global user.name 123a.aladov@codefresh.io>
2020-10-30 15:25:34 +0200

    CR-1352 - KUBECONFIG

--- installer/cmd/uninstall.go
+++ installer/cmd/uninstall.go
@@ -13,6 +13,7 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	"os"
 	"os/user"
 	"path"
 )
@@ -114,7 +115,10 @@ func init() {
 	var kubeConfigPath string
 	currentUser, _ := user.Current()
 	if currentUser != nil {
-		kubeConfigPath = path.Join(currentUser.HomeDir, ".kube", "config")
+		kubeConfigPath = os.Getenv("KUBECONFIG")
+		if kubeConfigPath == "" {
+			kubeConfigPath = path.Join(currentUser.HomeDir, ".kube", "config")
+		}
 	}
 
 	uninstallCmd.Flags().StringVar(&installCmdOptions.Kube.ConfigPath, "kubeconfig", kubeConfigPath, "Path to kubeconfig for retrieve contexts")
