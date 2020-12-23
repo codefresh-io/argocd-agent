4687ebed1e98092cabe65b0582a400567659f16a
olegz-codefresh <olegz@codefresh.io>
2020-12-23 17:34:34 +0200

    Link to gitops view

--- controller/pkg/questionnaire/kube.go
+++ controller/pkg/questionnaire/kube.go
@@ -2,7 +2,7 @@ package questionnaire
 
 import (
 	"github.com/codefresh-io/argocd-listener/controller/pkg/install"
-	"github.com/codefresh-io/argocd-listener/controller/pkg/kube"
+	"github.com/codefresh-io/argocd-listener/installer/pkg/kube"
 	"github.com/codefresh-io/argocd-listener/installer/pkg/prompt"
 )
 
