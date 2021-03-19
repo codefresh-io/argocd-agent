41ac2eba3749bba764347fbaa21252b009c3e81a
pashavictorovich <pavel@codefresh.io>
2021-03-19 20:59:06 +0200

    not remove , we still will have issues with huge resources trees

--- agent/pkg/event_handler/rollout.go
+++ agent/pkg/event_handler/rollout.go
@@ -1,7 +1,10 @@
 package event_handler
 
 import (
+	"github.com/codefresh-io/argocd-listener/agent/pkg/argo"
 	"github.com/codefresh-io/argocd-listener/agent/pkg/codefresh"
+	"github.com/codefresh-io/argocd-listener/agent/pkg/logger"
+	"github.com/codefresh-io/argocd-listener/agent/pkg/transform"
 	codefreshSdk "github.com/codefresh-io/go-sdk/pkg/codefresh"
 )
 
@@ -25,24 +28,24 @@ func (rolloutHandler *RolloutHandler) Handle(rollout interface{}) error {
 		return err
 	}
 
-	//resources, err := argo.GetInstance().GetResourceTreeAll(env.Name)
-	//if err != nil {
-	//	return err
-	//}
+	resources, err := argo.GetInstance().GetResourceTreeAll(env.Name)
+	if err != nil {
+		return err
+	}
 
-	//appResources := transform.GetApplicationResourcesTransformer().Transform(resources)
-	//if appResources != nil {
-	//	applicationResources := &codefreshSdk.ApplicationResources{
-	//		Name:      env.Name,
-	//		HistoryId: env.HistoryId,
-	//		Revision:  env.SyncRevision,
-	//		Resources: appResources,
-	//	}
-	//	logger.GetLogger().Infof("Send application resources for app %s with history %v", env.Name, env.HistoryId)
-	//	err = codefresh.GetInstance().SendApplicationResources(applicationResources)
-	//} else {
-	//	logger.GetLogger().Infof("Skip send application resources for app %s with history %v, because resources not exists", env.Name, env.HistoryId)
-	//}
+	appResources := transform.GetApplicationResourcesTransformer().Transform(resources)
+	if appResources != nil {
+		applicationResources := &codefreshSdk.ApplicationResources{
+			Name:      env.Name,
+			HistoryId: env.HistoryId,
+			Revision:  env.SyncRevision,
+			Resources: appResources,
+		}
+		logger.GetLogger().Infof("Send application resources for app %s with history %v", env.Name, env.HistoryId)
+		err = codefresh.GetInstance().SendApplicationResources(applicationResources)
+	} else {
+		logger.GetLogger().Infof("Skip send application resources for app %s with history %v, because resources not exists", env.Name, env.HistoryId)
+	}
 
 	return err
 }
