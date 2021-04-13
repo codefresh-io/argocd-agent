62a7a99c42247c5dbf3cd6105272bfe48ce6aa38
pashavictorovich <pavel@codefresh.io>
2021-04-13 22:21:59 +0300

    Revert "remove queue"
    This reverts commit 727989dc

--- agent/pkg/startup/runner.go
+++ agent/pkg/startup/runner.go
@@ -5,6 +5,7 @@ import (
 	"github.com/codefresh-io/argocd-listener/agent/pkg/api/codefresh"
 	"github.com/codefresh-io/argocd-listener/agent/pkg/events"
 	"github.com/codefresh-io/argocd-listener/agent/pkg/infra/logger"
+	"github.com/codefresh-io/argocd-listener/agent/pkg/infra/queue"
 	"github.com/codefresh-io/argocd-listener/agent/pkg/scheduler"
 	"github.com/codefresh-io/argocd-listener/agent/pkg/watch"
 )
@@ -47,5 +48,9 @@ func (runner *Runner) Run() error {
 	if err != nil {
 		logger.GetLogger().Errorf("Failed to run sync handler, reason %v", err)
 	}
+
+	queueProcessor := queue.EnvQueueProcessor{}
+	go queueProcessor.Run()
+
 	return watch.Watch()
 }
