498f8585a409c7ab35c545164be8619820499dfe
pashavictorovich <pavel@codefresh.io>
2021-04-13 18:57:45 +0300

    add argo resource test

--- agent/pkg/service/argo_resource_test.go
+++ agent/pkg/service/argo_resource_test.go
@@ -5,6 +5,11 @@ import (
 	"testing"
 )
 
+var _ = func() bool {
+	testing.Init()
+	return true
+}()
+
 func TestArgoResourceIdentifyChangedResources(t *testing.T) {
 	service := NewArgoResourceService()
 
