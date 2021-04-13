727989dc42f08af3d702c9e0b4881a1048c4b386
pashavictorovich <pavel@codefresh.io>
2021-04-13 22:19:50 +0300

    remove queue

--- agent/pkg/startup/runner.go
+++ agent/pkg/startup/runner.go
@@ -5,7 +5,6 @@ import (
 	"github.com/codefresh-io/argocd-listener/agent/pkg/api/codefresh"
 	"github.com/codefresh-io/argocd-listener/agent/pkg/events"
 	"github.com/codefresh-io/argocd-listener/agent/pkg/infra/logger"
-	"github.com/codefresh-io/argocd-listener/agent/pkg/infra/queue"
 	"github.com/codefresh-io/argocd-listener/agent/pkg/scheduler"
 	"github.com/codefresh-io/argocd-listener/agent/pkg/watch"
 )
@@ -48,9 +47,5 @@ func (runner *Runner) Run() error {
 	if err != nil {
 		logger.GetLogger().Errorf("Failed to run sync handler, reason %v", err)
 	}
-
-	queueProcessor := queue.EnvQueueProcessor{}
-	go queueProcessor.Run()
-
 	return watch.Watch()
 }
