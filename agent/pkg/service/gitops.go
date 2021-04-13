b4b0fdeda454757fc485aa51f8b3ea255178342b
pashavictorovich <pavel@codefresh.io>
2021-04-13 22:48:21 +0300

    remove useless code

--- agent/pkg/service/gitops.go
+++ agent/pkg/service/gitops.go
@@ -1,10 +1,7 @@
 package service
 
 import (
-	"github.com/codefresh-io/argocd-listener/agent/pkg/api/argo"
-	"github.com/codefresh-io/argocd-listener/agent/pkg/transform/env"
 	codefreshSdk "github.com/codefresh-io/go-sdk/pkg/codefresh"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
 type Gitops interface {
@@ -19,16 +16,17 @@ func NewGitopsService() Gitops {
 }
 
 func (gitops *gitops) MarkEnvAsRemoved(obj interface{}) (error, *codefreshSdk.Environment) {
-	envTransformer := env.GetEnvTransformerInstance(argo.GetInstance())
-	err, env := envTransformer.PrepareEnvironment(obj.(*unstructured.Unstructured).Object)
-	if err != nil {
-		return err, env
-	}
-
-	env.HealthStatus = "Deleted"
-
+	//disable for now
+	//envTransformer := env.GetEnvTransformerInstance(argo.GetInstance())
+	//err, env := envTransformer.PrepareEnvironment(obj.(*unstructured.Unstructured).Object)
+	//if err != nil {
+	//	return err, env
+	//}
+	//
+	//env.HealthStatus = "Deleted"
+	//
 	//err = events.GetRolloutEventHandlerInstance().Handle(env)
-
+	//
 	//if err != nil {
 	//	return err, nil
 	//}
