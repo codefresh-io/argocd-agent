94662c39ee90ad734adae67b03f508014f674676
pashavictorovich <pavel@codefresh.io>
2021-04-13 22:25:34 +0300

    Revert "remove queue"
    This reverts commit a3e173ea

---
+++ agent/pkg/infra/queue/queue_processor.go
@@ -0,0 +1,59 @@
+package queue
+
+import (
+	"github.com/codefresh-io/argocd-listener/agent/pkg/api/argo"
+	"github.com/codefresh-io/argocd-listener/agent/pkg/events"
+	"github.com/codefresh-io/argocd-listener/agent/pkg/infra/logger"
+	env2 "github.com/codefresh-io/argocd-listener/agent/pkg/transform/env"
+	"github.com/codefresh-io/argocd-listener/agent/pkg/util"
+	"github.com/codefresh-io/argocd-listener/agent/pkg/util/comparator"
+	codefreshSdk "github.com/codefresh-io/go-sdk/pkg/codefresh"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"time"
+)
+
+type QueueProcessor interface {
+	Run()
+}
+
+type EnvQueueProcessor struct {
+}
+
+var envQueueProcessor *EnvQueueProcessor
+
+func (processor *EnvQueueProcessor) New() QueueProcessor {
+	if envQueueProcessor == nil {
+		envQueueProcessor = &EnvQueueProcessor{}
+	}
+	return envQueueProcessor
+}
+
+func updateEnv(obj *unstructured.Unstructured) (error, *codefreshSdk.Environment) {
+	envTransformer := env2.GetEnvTransformerInstance(argo.GetInstance())
+	err, env := envTransformer.PrepareEnvironment(obj.Object)
+	if err != nil {
+		return err, env
+	}
+
+	envComparator := comparator.EnvComparator{}
+
+	err = util.ProcessDataWithFilter("environment", &env.Name, env, envComparator.Compare, func() error {
+		return events.GetRolloutEventHandlerInstance().Handle(env)
+	})
+
+	return err, env
+}
+
+func (processor *EnvQueueProcessor) Run() {
+	itemQueue := GetInstance()
+	for true {
+		if itemQueue.Size() > 0 {
+			item := itemQueue.Dequeue()
+			err, _ := updateEnv(item)
+			if err != nil {
+				logger.GetLogger().Errorf("Failed to update environment, reason: %v", err)
+			}
+		}
+		time.Sleep(1 * time.Second)
+	}
+}
