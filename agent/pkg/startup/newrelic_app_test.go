06fa7973d586a4d06dbef571dc88ffb3f8628c80
olegz-codefresh <olegz@codefresh.io>
2021-05-20 17:46:25 +0300

    CR-4496
    NewRelicApp

--- agent/pkg/startup/newrelic_app_test.go
+++ agent/pkg/startup/newrelic_app_test.go
@@ -9,7 +9,7 @@ var _ = func() bool {
 	return true
 }()
 
-func TestInitNewRelic(t *testing.T) {
+func TestInitNewRelicWithKey(t *testing.T) {
 	input := &Input{
 		argoHost:                 "http://argo-host",
 		argoToken:                "1234",
@@ -29,5 +29,28 @@ func TestInitNewRelic(t *testing.T) {
 	err := NewNewrelicApp(input).Init()
 	if err == nil {
 		t.Error("TestInitNewRelic should be failed")
+	}
+}
+
+func TestInitNewRelicWithoutKey(t *testing.T) {
+	input := &Input{
+		argoHost:                 "http://argo-host",
+		argoToken:                "1234",
+		argoUsername:             "",
+		argoPassword:             "",
+		codefreshToken:           "token",
+		codefreshHost:            "http://cf-host",
+		newRelicLicense:          "",
+		codefreshIntegrationName: "test",
+		applications:             nil,
+		agentVersion:             "124",
+		gitIntegration:           "integration",
+		password:                 "",
+		syncMode:                 "AUTOSYNC",
+	}
+	NewInputStore(input).Store()
+	err := NewNewrelicApp(input).Init()
+	if err != nil {
+		t.Error("TestInitNewRelic should not be failed")
 	}
 }
