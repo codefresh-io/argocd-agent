31e5dc71a615972e064c6125839a404f4fe8da96
olegz-codefresh <olegz@codefresh.io>
2021-05-24 17:07:11 +0300

    CR-4496
    NewRelicApp

--- agent/pkg/infra/newrelic/newrelic_app.go
+++ agent/pkg/infra/newrelic/newrelic_app.go
@@ -3,8 +3,14 @@ package newrelic
 import (
 	"fmt"
 	"github.com/codefresh-io/argocd-listener/agent/pkg/infra/logger"
+	"github.com/codefresh-io/argocd-listener/agent/pkg/util"
 	nr "github.com/newrelic/go-agent"
 )
+
+type EventParams struct {
+	Owner string `json:"owner"`
+	Repo  string `json:"repo"`
+}
 
 var newrelicApp nr.Application
 
@@ -19,9 +25,11 @@ func Init(newRelicLicense string, envName string) error {
 	return nil
 }
 
-func RecordCustomEvent(eventType string, params map[string]interface{}) error {
+func RecordCustomEvent(eventType string, params EventParams) error {
+	var nrParams map[string]interface{}
+	util.Convert(params, &nrParams)
 	if newrelicApp == nil {
-		err := newrelicApp.RecordCustomEvent(fmt.Sprintf("GitopsDashboardGit::%s", eventType), params)
+		err := newrelicApp.RecordCustomEvent(fmt.Sprintf("GitopsDashboardGit::%s", eventType), nrParams)
 		if err != nil {
 			logger.GetLogger().Errorf("Newrelic RecordCustomEvent \"%s\" error %s", eventType, err.Error())
 			return err
