bf306f0f29299a913a25a9facc164609c26d1e0c
olegz-codefresh <olegz@codefresh.io>
2021-09-17 16:06:48 +0300

    CR-5892

--- agent/pkg/service/env_transformer.go
+++ agent/pkg/service/env_transformer.go
@@ -189,6 +189,7 @@ func (envTransformer *EnvTransformer) PrepareEnvironment(app argoSdk.ArgoApplica
 
 	if commit != nil {
 		logger.GetLogger().Infof("Retrieve commit message \"%s\" for repo \"%s\" ", *commit.Message, repoUrl)
+
 		env.Commit = codefreshSdk.Commit{
 			Time:    commit.Time,
 			Message: commit.Message,
